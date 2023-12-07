5a4d5e32ab00c39f26715bd2c32786cc0ffbd34e
Pedro Barreto <pedro.barreto.ne@gmail.com>
2023-12-07 19:27:28 -0300

    Ex1

---
+++ React/OneBitCode/Ex/9-Ex1/Ex1/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+
+  return (
+    <div>
+      <img src="react.png" alt="" />
+
+      <h1>React</h1>
+      <p>lordasdasdas ASFbadsj  dlajskj SADIJOBDFASOINFDSAIO AF OFUADNFIASDBNIL afbonifniaos</p>
+      <button>Aprenda</button>
+      <button>API</button>
+      <hr />
+
+      <h1>Próximos passos</h1>
+      <ul>
+        <li>aqui</li>
+        <li>aqui</li>
+        <li>aqui</li>
+        <li>aqui</li>
+      </ul>
+
+    </div>
+    
+  )
+}
+
+export default App
