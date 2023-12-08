5f46535345a89d1ffb514f200fd719a6112739d9
Pedro <pedro.barreto.ne@gmail.com>
2023-12-08 16:43:37 -0300

    Ex2 - Correcoes

--- React/OneBitCode/Ex/Ex2/ex2/src/components/Card/TextCard/index.jsx
+++ React/OneBitCode/Ex/Ex2/ex2/src/components/Card/TextCard/index.jsx
@@ -1,9 +1,11 @@
+import styles from "./styles.module.css"
+
 export default function TextCard(){
   return (
-    <div>
-      <h1>Pôster</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolores quo iste, fuga, totam est voluptas et cum numquam eligendi doloribus assumenda, quis corrupti labore! Corrupti voluptatum autem sunt ipsam!</p>
-      <button>Comprar agora</button>
+    <div className={styles}>
+      <h1 className={styles.title}>Pôster</h1>
+      <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolores quo iste, fuga, totam est voluptas et cum numquam eligendi doloribus assumenda, quis corrupti labore! Corrupti voluptatum autem sunt ipsam!</p>
+      <button className={styles.button}>Comprar agora</button>
     </div>
   )
 }
--- React/OneBitCode/Ex/Ex2/ex2/src/components/Card/index.jsx
+++ React/OneBitCode/Ex/Ex2/ex2/src/components/Card/index.jsx
@@ -1,9 +1,11 @@
 import TextCard from "./TextCard"
+import banner from "../../assets/banner.jpg"
+import style from "./style.module.css"
 
 export default function Card(){
   return (
-    <div style={{display: "flex"}}>
-      <img src="../../../public/banner.jpg" alt="Banner" />
+    <div className={style.container}>
+      <img src={banner} alt="Banner" className={style.poster} />
       <TextCard />
     </div>
   )
