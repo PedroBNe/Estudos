f6af8a161136f69a14d079dfe7831ef0d5e71d7d
Pedro <pedro.barreto.ne@gmail.com>
2023-12-11 15:22:15 -0300

    21- children

--- React/OneBitCode/Ex/Ex3/Ex3/src/components/Card/index.jsx
+++ React/OneBitCode/Ex/Ex3/Ex3/src/components/Card/index.jsx
@@ -1,18 +1,19 @@
 import Button from "../Button"
+import Title from "../Title"
+import Section from "./Section"
 import styles from "./styles.module.css"
 
 export default function Card({image, name, bio, number, email, git, link, twi}){
   return (
     <div className={styles.Card}>
       <img src={image} alt="Foto de Perfil" />
-      <h1>{name}</h1>
-      <hr />
-      <p>{bio}</p>
-      <hr />
-      <p>{number}</p>
-      <hr />
-      <p>{email}</p>
-      <hr />
+      <Title>
+        <span>{name}</span>
+        <button>Seguir</button>
+      </Title>
+      <Section>{bio}</Section>
+      <Section>{number}</Section>
+      <Section>{email}</Section>
       <Button type="GitHub" link={git} />
       <Button type="LinkedIn" link={link} />
       <Button type="Twitter" link={twi} />
