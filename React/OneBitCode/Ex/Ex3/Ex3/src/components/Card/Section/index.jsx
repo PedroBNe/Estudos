f09a4b0094fd255ed986a755e3ac8b67bd2578ce
Pedro <pedro.barreto.ne@gmail.com>
2023-12-12 11:24:04 -0300

    23- Eventos

--- React/OneBitCode/Ex/Ex3/Ex3/src/components/Card/Section/index.jsx
+++ React/OneBitCode/Ex/Ex3/Ex3/src/components/Card/Section/index.jsx
@@ -1,6 +1,11 @@
+import styles from "./styles.module.css"
+
 export default function Section(props){
     return(
-        <div>
+        <div
+            {...props}
+            className={`${styles.wrapper} ${props.className}`}
+        >
             {props.children}
         </div>
     )
--- React/OneBitCode/Ex/Ex3/Ex3/src/components/Card/index.jsx
+++ React/OneBitCode/Ex/Ex3/Ex3/src/components/Card/index.jsx
@@ -3,20 +3,36 @@ import Title from "../Title"
 import Section from "./Section"
 import styles from "./styles.module.css"
 
+function handleClick(ev){
+  console.log(ev)
+  alert("teste")
+}
+
 export default function Card({image, name, bio, number, email, git, link, twi}){
+
   return (
     <div className={styles.Card}>
       <img src={image} alt="Foto de Perfil" />
       <Title>
         <span>{name}</span>
-        <button>Seguir</button>
+        <button
+        className={styles.followButton}
+          onClick={handleClick}
+        >Seguir</button>
       </Title>
       <Section>{bio}</Section>
       <Section>{number}</Section>
       <Section>{email}</Section>
-      <Button type="GitHub" link={git} />
-      <Button type="LinkedIn" link={link} />
-      <Button type="Twitter" link={twi} />
+      <Section
+        className={styles.links}
+        id="links-section"
+        data-test="some value"
+        arial-label="social links"
+      >
+        <Button type="GitHub" link={git} />
+        <Button type="LinkedIn" link={link} />
+        <Button type="Twitter" link={twi} />
+      </Section>
     </div>
   )
 }
