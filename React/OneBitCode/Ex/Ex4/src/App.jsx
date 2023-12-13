bc20de7735adde68ef48db3ed6bf6ab0e22268ab
Pedro <pedro.barreto.ne@gmail.com>
2023-12-13 15:14:42 -0300

    31- Renderizacao condicional

--- React/OneBitCode/Ex/Ex4/src/App.jsx
+++ React/OneBitCode/Ex/Ex4/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   const [password, setPassword] = useState("")
   const [copy, setCopy] = useState("Copiar")
-  const [passwordSize, setPasswordSize] = useState(12)
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
+
+  const passwordSize = showInput ? customSize : 12
 
   function generate(){
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -29,9 +32,20 @@ function App() {
     <div className='app'>
       <h1>Gerador de senhas</h1>
       <div>
-        <label htmlFor="passwordSize">Tamanho:</label>
-        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+        <label htmlFor="showInput">Customizar tamanho</label>
+        <input
+          type="checkbox"
+          id='showInput'
+          value={showInput}
+          onChange={() => setShowInput(currentState => !currentState)}
+        />
       </div>
+      {showInput ? (
+        <div>
+          <label htmlFor="passwordSize">Tamanho:</label>
+          <Input passwordSize={customSize} setPasswordSize={setCustomSize} />
+        </div>
+      ) : null}
       <button onClick={generate}>Gerar senha de {passwordSize} caracteres!</button>
       <button onClick={copyToClipboard}>{copy}</button>
       <p>{password}</p>
