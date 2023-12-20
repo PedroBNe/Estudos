77c25dbcc379341f50739bc7890685fa85c9ced3
Pedro <pedro.barreto.ne@gmail.com>
2023-12-20 18:02:52 -0300

    39-useEffect-com-http

---
+++ React/OneBitCode/39-useEffectHTTP/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+
+async function fetchPokemon(){
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+  const data = await response.json()
+  return data.results
+}
+
+export default function App(){
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetchPokemon().then(results => {
+      console.log("Requisição realizada")
+      console.log(results)
+      setPokemon(results)
+    })
+  }, [])
+
+  return (
+    <div className="app">
+      <div>
+        <h2>Pokémon</h2>
+        <ul className="pokemon">
+          {pokemon.map(mon => (
+            <li key={mon.name}>
+              <span>{mon.name}</span>
+              <button>
+                Ver detalhes
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
