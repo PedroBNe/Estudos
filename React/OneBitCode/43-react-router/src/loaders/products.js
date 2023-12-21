98cee2c582272d26e45fc8c6ccac5e4fbc374426
Pedro <pedro.barreto.ne@gmail.com>
2023-12-21 15:36:53 -0300

    45-DataFetchingError

---
+++ React/OneBitCode/43-react-router/src/loaders/products.js
@@ -0,0 +1,10 @@
+import products from "../database.json";
+
+export default function loadProduct({ params }){
+  const product = products.find(p => p.id === +params.productId)
+
+  if (!product) {
+    throw new Response("404 not found", { status: 404 });
+  }
+  return product
+}
